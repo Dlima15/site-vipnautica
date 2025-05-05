c76de5069f5a88bb805f3feec02b08a40f818e7f
Dlima15 <danilo.lima7456@gmail.com>
2025-05-05 17:54:02 -0300

    Scroll automatico

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,4 +8,20 @@ function scrollCarousel(direction) {
 
 
 // fim da funionalidade
+ 
 
+//Função scroll site 
+
+// scroll.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 80, // ajuste se o header for fixo
+        behavior: "smooth"
+      });
+    }
+  });
+});
