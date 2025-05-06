1179250a562ab13895e370353d14230990233a49
Dlima15 <danilo.lima7456@gmail.com>
2025-05-06 14:25:16 -0300

    dev pagina anuncio

--- assets/js/anuncio.js
+++ assets/js/anuncio.js
@@ -1,1 +1,18 @@
 // JS da Página de Anúncio
+console.log("teste js")
+// scroll header
+
+// scroll.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (target) {
+        window.scrollTo({
+          top: target.offsetTop - 80, // ajuste se o header for fixo
+          behavior: "smooth"
+        });
+      }
+    });
+  });
+  
