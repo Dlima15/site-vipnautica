2577055a4ada09ce01e81d0856f2d890b9bb88ea
Dlima15 <danilo.lima7456@gmail.com>
2025-06-04 16:32:18 -0300

    capa site novas

--- assets/js/home.js
+++ assets/js/home.js
@@ -15,7 +15,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         const target = document.querySelector(this.getAttribute("href"));
         if (target) {
             window.scrollTo({
-                top: target.offsetTop - 80, // ajuste se o header for fixo
+                top: target.offsetTop - 80, // 
                 behavior: "smooth"
             });
         }
